5eb279e74359d49f17c1da098998521f66032308
Willow Hayward <willow@whc.fyi>
2024-05-24 22:56:34 +1000

    test(angular,utils): fixed some tests ay

--- libs/angular/src/lib/api.service.spec.ts
+++ libs/angular/src/lib/api.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
+import {
+    HttpClientTestingModule,
+    HttpTestingController,
+} from '@angular/common/http/testing';
+import { LipwigApiService } from './api.service';
 
-import { ApiService } from './api.service';
-
-describe('ApiService', () => {
-    let service: ApiService;
+describe('LipwigApiService', () => {
+    let service: LipwigApiService;
+    let httpMock: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(ApiService);
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [LipwigApiService],
+        });
+
+        service = TestBed.inject(LipwigApiService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify(); // Ensure that there are no outstanding requests
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should throw an error if the API URL is not set', async () => {
+        await expect(service.query('test')).rejects.toThrow(
+            'Lipwig API URL not set'
+        );
+    });
+
+    it('should send a GET request to the correct URL', async () => {
+        service.setUrl('http://localhost:3000');
+        service.query('test').then();
+
+        const req = httpMock.expectOne(
+            'http://localhost:3000/api/query?code=test'
+        );
+        expect(req.request.method).toBe('GET');
+        req.flush({});
     });
 });
--- libs/utils/src/lib/utils.spec.ts
+++ libs/utils/src/lib/utils.spec.ts
@@ -1,7 +1,23 @@
-import { utils } from './utils';
+import { generateString } from './utils';
 
-describe('utils', () => {
-    it('should work', () => {
-        expect(utils()).toEqual('utils');
+describe('generateString', () => {
+    it('should generate a string of default length 4', () => {
+        const result = generateString();
+        expect(result).toHaveLength(4);
+    });
+
+    it('should generate a string of specified length', () => {
+        const result = generateString(6);
+        expect(result).toHaveLength(6);
+    });
+
+    it('should generate a string using default characters', () => {
+        const result = generateString();
+        expect(result).toMatch(/^[A-Z]{4}$/);
+    });
+
+    it('should generate a string using specified characters', () => {
+        const result = generateString(4, '123');
+        expect(result).toMatch(/^[123]{4}$/);
     });
 });
