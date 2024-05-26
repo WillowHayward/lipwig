90d286932a960976839d257a1899579c0719680f
Willow Hayward <willow@whc.fyi>
2024-05-27 00:39:23 +1000

    chore(model): exported missing server common model

--- libs/model/src/lib/common/common.exports.ts
+++ libs/model/src/lib/common/common.exports.ts
@@ -1,6 +1,7 @@
 import { BaseClientErrorCode, BaseHostErrorCode, BaseJoinErrorCode, BasePollErrorCode, BaseRejoinErrorCode, GenericErrorCode } from './error.model';
 
 export { BaseMessage } from './common.model'; // TODO: Confirm if this is used anywhere
+export * from './server.common.model';
 
 export * from './logs.model'; // TODO: Rework this to be cleaner, or move to server (or standardise with js)
 
